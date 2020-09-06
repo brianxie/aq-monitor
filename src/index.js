c2dff2a33a58822f3536ffec933a0e59a0236c1d
Brian Xie <briancxie@gmail.com>
2020-09-06 11:39:17 -0700

    Handle errors!

--- src/index.js
+++ src/index.js
@@ -23,7 +23,8 @@ class Status extends React.Component {
     return (
       <div>
         {this.props.statusText}
-        {"\nYour position: "}
+        {<br />}
+        {"Your position: "}
         {this.props.position}
       </div>
     );
@@ -67,19 +68,42 @@ class Monitor extends React.Component {
     return stats.v1;
   }
 
+  checkResponseOk(response) {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response;
+  }
+
   // Reads raw sensor results from purpleair.
+  // Future function.
   getRawResults() {
     const srcUrl = "https://www.purpleair.com/json";
     return fetch(srcUrl)
+      .then(response => this.checkResponseOk(response))
       .then(response => response.json())
       .then(jsonObj => jsonObj.results)
       .then(results => results.map(result => this.createTaggedResult(result)));
+  }
+
+  handleResultError(error) {
+    const resultErrorString = "Error in fetching sensors: " + error.message;
+    // Rethrow the error for subsequent nodes.
+    throw new Error(resultErrorString);
+  }
+
+  handlePositionError(error) {
+    const positionErrorString = "Error in fetching position: " + error.message;
+    this.setState({position: positionErrorString});
+    // Rethrow the error for subsequent nodes.
+    throw new Error(positionErrorString);
   }
 
   // Fetches location and sensor readings, and updates the status.
   updateStatusAndButton() {
     this.setState({count: this.state.count + 1});
     this.setState({statusText: "Request pending..."});
+    this.setState({position: "Position pending..."});
 
     // Issue requests for position and sensor readings.
     var positionPromise = function (options) {
@@ -89,17 +113,31 @@ class Monitor extends React.Component {
     }();
     var rawResultsPromise = this.getRawResults();
 
-    // Update position in UI.
-    positionPromise
-      .then(position =>
-        position.coords.latitude.toString() + ", " + position.coords.longitude.toString())
-      .then(positionString => this.setState({position: positionString}));
+    // Check for errors in fetching position, and update UI immediately if possible.
+    positionPromise = positionPromise
+      .then(position => {
+        var positionString =
+          position.coords.latitude.toString()
+            + ", "
+            + position.coords.longitude.toString();
+        this.setState({position: positionString});
+        // Return the original position, so that subsequent logic can work with it.
+        // Pretty-print is only for UI purposes.
+        return position;
+      })
+      .catch(error => this.handlePositionError(error));
+
+    // Check for errors with the sensor reading per se, because Promise.all fails fast.
+    // The status value can't be updated until we have both position and sensor readings.
+    rawResultsPromise = rawResultsPromise
+      .catch(error => this.handleResultError(error));
 
     // Compute PM2.5 and update UI.
     Promise.all([rawResultsPromise, positionPromise])
       .then(promises => this.getSortedResults(promises[0], promises[1]))
       .then(filteredResults => this.handleFilteredResults(filteredResults))
-      .then(status => this.setState({statusText: status}));
+      .then(status => this.setState({statusText: status}))
+      .catch(error => this.setState({statusText: error.message}));
   }
 
   renderStatus() {
