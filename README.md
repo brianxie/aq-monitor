37ef8c0315ce044f83a6684cc178efdf7d02969b
Brian Xie <briancxie@gmail.com>
2020-09-06 16:22:01 -0700

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-`HTTPS=true npm start`, then open in [https://localhost:3000](https://localhost:3000).
---
+++ precache-manifest.8be16e45f11ecb5e950f732c5a4a8652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1146d49ed2988975b80b55ee7d421c6",
+    "url": "/aq-monitor/index.html"
+  },
+  {
+    "revision": "a249fa18a582b29f888d",
+    "url": "/aq-monitor/static/css/main.17f059be.chunk.css"
+  },
+  {
+    "revision": "54cc7a933e52b8375445",
+    "url": "/aq-monitor/static/js/2.ec6d71b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aq-monitor/static/js/2.ec6d71b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a249fa18a582b29f888d",
+    "url": "/aq-monitor/static/js/main.f8d6d1fa.chunk.js"
+  },
+  {
+    "revision": "7434c941c1ec29e74772",
+    "url": "/aq-monitor/static/js/runtime-main.44834d90.js"
+  }
+]);
